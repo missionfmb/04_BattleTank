25983e72b72fa9a0bec514188d0e3b5db66fe439
DESKTOP-ETJGMAL\Chris <chicken_poo7171@hotmail.co.uk>
2017-11-21 14:59:11 +0000

    Added TankAIController and aiming function.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,11 +1,59 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "Engine/World.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
 
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	UE_LOG(LogTemp, Warning, TEXT("Player controller begin play"));
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No possessed tank."));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Possessed tank is %s"), *ControlledTank->GetName());
+	}
+}
+
+void ATankPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick( DeltaSeconds );
+	AimTowardsCrosshair();
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; // Out parameter
+	
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+	}
+
+	// Get world location if linetrace through crosshair
+	// If it hits the landscape
+		// Tell controlled tank to aim at this point
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	//raycast
+	// set raycast hitlocation to OutHitLocation
+	
+	OutHitLocation = FVector(1.0);
+	return true; 
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -18,5 +18,16 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 public:
 
 	ATank* GetControlledTank() const;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick( float DeltaSeconds ) override;
+
+	void AimTowardsCrosshair();
+
+private:
+
+	
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
 	
 };
